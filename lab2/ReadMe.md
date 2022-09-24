fe0819a69e40187674ca1c0102f6e177aed9249a
firebenderyt123 <firebenderyt@gmail.com>
2022-09-25 00:00:01 +0300

    lab2

---
+++ lab2/ReadMe.md
@@ -0,0 +1,4 @@
+# Sources
+
+https://ela.kpi.ua/bitstream/123456789/43381/1/MOP_Praktykum.pdf - page 18
+https://pythonru.com/osnovy/formatirovanie-v-python-s-pomoshhju-format
---
+++ lab2/lab2.py
@@ -0,0 +1,38 @@
+import math
+
+def toFixed(num, digits = 2):
+    return f"{num:6.{digits}f}".format(12)
+
+def f(x):
+    return pow(x, 2)
+    # return pow(100-x, 2)
+    # return math.exp(2-x)+x*math.atan(x)-0.5*math.log10(1+pow(x, 2))
+
+def calc(a, b, eps):
+    L = b - a
+    while L > eps:
+        x1 = a + 0.25 * L
+        x2 = b - 0.25 * L
+        xm = (a + b) / 2
+        print(
+            'x1:', toFixed(x1), 'xm:', toFixed(xm),
+            'x2:', toFixed(x2), 'f(x1):', toFixed(f(x1)),
+            'f(x2):',toFixed(f(x2)), 'a:', toFixed(a),
+            'b:', toFixed(b), 'L:', toFixed(L)
+        )
+        if (f(x1) < f(xm)):
+            b = xm
+            xm = x1
+        else:
+            if (f(x2) < f(xm)):
+                a = xm
+                xm = x2
+            else:
+                a = x1
+                b = x2
+        L = b - a
+    return xm, f(xm), L
+
+if __name__ == '__main__':
+    x, y, L = calc(-10, 15, 1e-3)
+    print('\n\nx:', x, 'y:', y, 'Точність:', L)
